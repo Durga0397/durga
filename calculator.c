ebc323bb3fd3e85de878757811a2dcb0c9635ce0
Durga0397 <durga0397sawant@gmail.com>
2019-12-05 06:32:51 -0800

    success

---
+++ calculator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    char operator;
+    double f,s;
+    printf("Enter an operator (+, -, *,): ");
+    scanf("%c", &operator);
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &f, &s);
+    switch (operator)
+	{
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", f, s, f + s);
+        break;
+    case '-':
+        printf("%.1lf - %.1lf = %.1lf", f, s, f - s);
+        break;
+    case '*':
+        printf("%.1lf * %.1lf = %.1lf", f, s, f * s);
+        break;
+    case '/':
+        printf("%.1lf / %.1lf = %.1lf", f, s, f / s);
+        break;
+          default:
+        printf("Error");
+    }
+    return 0;
